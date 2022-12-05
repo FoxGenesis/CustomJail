0c1a1600de04e1934480145d04edfa4beed927b0
Ashley <ashley@foxgenesis.net>
2022-12-04 18:03:31 -0600

    initial setup

---
+++ src/net/foxgenesis/customjail/CustomJailPlugin.java
@@ -0,0 +1,41 @@
+package net.foxgenesis.customjail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.foxgenesis.watame.WatameBot;
+import net.foxgenesis.watame.WatameBot.ProtectedJDABuilder;
+import net.foxgenesis.watame.plugin.IPlugin;
+import net.foxgenesis.watame.plugin.PluginProperties;
+
+@PluginProperties(name = "Custom Jailing", description = "A plugin to manage jailings", version = "0.0.1")
+public class CustomJailPlugin implements IPlugin {
+	/**
+	 * Logger
+	 */
+	private static final Logger logger = LoggerFactory.getLogger("Custom Jail");
+	
+	@Override
+	public void preInit() { 
+		
+	 }
+
+	@Override
+	public void init(ProtectedJDABuilder builder) { 
+		
+	 }
+
+	@Override
+	public void postInit(WatameBot bot) { 
+		
+	 }
+
+	@Override
+	public void onReady(WatameBot bot) { 
+		
+	 }
+	
+	@Override
+	public void close() throws Exception { 
+		
+	 }
+}
