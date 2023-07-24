665583a74d83a5d2ad5a722c301149f9b530b79e
Ashley <ashley@foxgenesis.net>
2023-07-24 16:59:18 -0500

    cleaned up requirements

--- src/module-info.java
+++ src/module-info.java
@@ -7,14 +7,13 @@ module watamebot.customjail {
 	exports net.foxgenesis.customjail.jail.event.impl;
 	exports net.foxgenesis.customjail.timer to quartz;
 
-	requires transitive java.desktop;
 	requires transitive net.dv8tion.jda;
-	requires transitive org.apache.commons.lang3;
-	requires org.jetbrains.annotations;
-	requires transitive org.slf4j;
 	requires transitive quartz;
 	requires transitive watamebot;
-	requires transitive com.zaxxer.hikari;
+
+	requires static org.jetbrains.annotations;
+
+	requires com.zaxxer.hikari;
 
 	provides net.foxgenesis.watame.plugin.Plugin with net.foxgenesis.customjail.CustomJailPlugin;
 }
